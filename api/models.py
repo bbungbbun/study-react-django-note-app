0b5bef93d964a0e07370c228127572d8acaf3415
bbungbbun <tnqkrdbsl@naver.com>
2023-11-01 15:21:48 +0900

    admin 계정 생성 및 모델 추가

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True) # auto_now 현재 시각
+    created = models.DateTimeField(auto_now_add=True) # auto_now_add 생성 시각
+
+    def __str__(self):
+        return self.body[0:50]
