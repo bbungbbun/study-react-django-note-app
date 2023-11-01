5d3017883d4de1375216dc2c9ff6116683caef34
bbungbbun <tnqkrdbsl@naver.com>
2023-11-01 16:50:40 +0900

    cor-headers 추가 및 화면에 데이터 불러오기

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const NotesListPage = () => {
+  let [notes, setNotes] = useState([])
+
+  useEffect(() => {
+      getNotes()
+  }, [])
+
+
+  let getNotes = async () => {
+
+      let response = await fetch('http://127.0.0.1:8000/api/notes/')
+      let data = await response.json()
+      setNotes(data)
+  }
+
   return (
-    <div>Notes</div>
+    <div className="notes-list">
+        {notes.map((note, index) => (
+            <h3 key={index}>{note.body}</h3>
+        ))}
+    </div>
   )
 }
 
